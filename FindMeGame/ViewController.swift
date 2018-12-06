72b6ec6b37890d4e9120e7b61c443aec227a2f38
Vsevolod Onishchenko <v.onishchenko@andersenlab.com>
2018-12-06 15:55:11 +0300

    view controller preparation

--- FindMeGame/ViewController.swift
+++ FindMeGame/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import MobileCoreServices
+import Vision
+import CoreML
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var objectLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var skipButton: UIButton!
+    
+    /// Перенести в презентер
+    var cameraLayer: CALayer!
+    var gameTimer: Timer!
+    var timeRemaining = 60
+    var currentScope = 0
+    var highScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        viewSetup()
     }
-
-
+    
+    private func viewSetup() {
+        
+        let backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        topView.backgroundColor = backgroundColor
+        bottomView.backgroundColor = backgroundColor
+        scoreLabel.text = "0"
+    }
+    
+    
 }
 
